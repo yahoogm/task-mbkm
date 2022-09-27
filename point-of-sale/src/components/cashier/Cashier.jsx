a15b50e2357b92f0beb92f67142cec9a6ae2bcef
yahoogm <yaho.garfield@gmail.com>
2022-09-27 21:00:47 +0700

    update state qty

--- point-of-sale/src/components/cashier/Cashier.jsx
+++ point-of-sale/src/components/cashier/Cashier.jsx
@@ -1,12 +1,27 @@
-import React, { useState } from 'react';
-import AddItems from '../additems/AddItems';
-import Items from '../items/Items';
+import React, { useState } from "react";
+import AddItems from "../additems/AddItems";
+import Items from "../items/Items";
 
 const Cashier = () => {
   const [items, setItems] = useState([]);
-  const [showAddItems, setShowAddItems] = useState(false);
+  const [showAddItems, setShowAddItems] = useState(true);
   const handleAddItems = (product) => {
     setItems([...items, { ...product }]);
+  };
+
+  const handleDelete = (id) => {
+    let updateList = items.filter((item) => {
+      return item.id !== id;
+    });
+    setItems(updateList);
+  };
+
+  const handleChangeQty = (id) => {
+    const newTask = items.map((item) => {
+      return item.id === id ? { ...item, qty: item.qty + 1 } : { ...item, qty: item.qty - 1 };
+    });
+    setItems(newTask);
+    console.log(newTask);
   };
 
   let harga = 0;
@@ -17,27 +32,31 @@ const Cashier = () => {
   return (
     <div>
       <button className="bg-blue-300 p-2 rounded-md shadow-md text-white" onClick={() => setShowAddItems(!showAddItems)}>
-        Add
+        {showAddItems ? "Hide items" : "Show Items"}
       </button>
       {showAddItems && <AddItems onHandleAddItems={handleAddItems} />}
 
-      <table className="w-full">
-        <thead>
+      <table className="w-full border">
+        <thead className="">
           <tr>
             <td>no</td>
-            <td>nama barang</td>
-            <td>quantity</td>
-            <td>harga</td>
-            <td>sub harga</td>
-            <td>hapus</td>
+            <td>Title</td>
+            <td>Qty</td>
+            <td>Price</td>
+            <td>Total</td>
+            <td>Action</td>
           </tr>
         </thead>
-        <tbody>
-          <Items items={items} />
-          <tr>
-            <td>total :{harga} </td>
-          </tr>
+        <tbody className="">
+          <Items items={items} handleDelete={handleDelete} handleChangeQty={handleChangeQty} />
         </tbody>
+      </table>
+      <table>
+        <thead>
+          <tr>
+            <td>Total : {harga} </td>
+          </tr>
+        </thead>
       </table>
     </div>
   );
--- point-of-sale/src/components/items/Items.jsx
+++ point-of-sale/src/components/items/Items.jsx
@@ -1,14 +1,17 @@
-const Items = ({ items }) => {
+const Items = ({ items, handleDelete, handleChangeQty }) => {
   return (
     <>
       {items.map((item, index) => (
-        <tr key={item.id}>
+        <tr key={item.id} className="border">
           <td>{index + 1}</td>
           <td>{item.title}</td>
-          <td>{item.qty}</td>
+
+          <td>
+            <input type="number" onChange={() => handleChangeQty(item.id)} value={item.qty}></input>
+          </td>
           <td>{item.price}</td>
           <td>{item.price * item.qty}</td>
-          <td>Hapus</td>
+          <td onClick={() => handleDelete(item.id)}>Delete</td>
         </tr>
       ))}
     </>
